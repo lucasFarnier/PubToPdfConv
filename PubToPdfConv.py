aff941db3ede65a7f75f8eca2b873ab095473ebb
l.farnier <lucasfarnier71@gmail.com>
2025-06-23 12:55:15 +0100

    converts to pdf format but not openable

--- PubToPdfConv.py
+++ PubToPdfConv.py
@@ -1,5 +1,24 @@
 import os
-for root, dirs, files in os.walk("C:/Users/L.Farnier/Desktop"):
+import comtypes.client
+
+publisher = comtypes.client.CreateObject("Publisher.Application")
+for root, dirs, files in os.walk(r"C:\Test"):
     for file in files:
         if file.endswith(".pub"):
-            print(os.path.join(root, file))
+            filePathInp = os.path.join(root, file)
+            filePathOut = os.path.join(root, f"{os.path.splitext(file)[0]}.pdf")
+            print(filePathInp)
+            print(filePathOut)
+
+            try:
+                publisher.Visible = True
+
+                doc = publisher.Open(filePathInp)
+                doc.ExportAsFixedFormat(Filename=filePathOut, Format=1)
+                doc.Close()
+
+                print(f"Conversion successful: {filePathOut}")
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+publisher.Quit()

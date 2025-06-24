f409bd271363554f0032912d9a4a02b7fb415ce0
l.farnier <lucasfarnier71@gmail.com>
2025-06-24 15:56:24 +0100

    can now convert from pdf to docx (can do publisher to docx by doing pub-pdf-docx)
    future steps improve efficiency instead of 2 large functions just have 1 with switch/if

--- PubToPdfConv.py
+++ PubToPdfConv.py
@@ -29,13 +29,14 @@ def browseFolders():
     for root, dirs, files in os.walk(folderSelected):
         for file in files:
             #if ones found adds it to the list and the gui
-            if file.lower().endswith(".pub"):
+            if file.lower().endswith(".pub") or file.lower().endswith(".pdf"):
                 pubFiles.append(os.path.join(root, file))
                 tree.insert("", "end", values=(os.path.join(root, file), "", "", ""))
 
     #if the list isnt empty then activate the convert button
     if len(pubFiles) != 0:
-        convertButton.configure(state="normal")
+        convertButtonPb.configure(state="normal")
+        convertButtonWd.configure(state="normal")
     #if its empty output error message box telling user, also keeps convert button inactive
     else:
         messagebox.showerror("No files found", "Folder doesnt contain any .pub (publisher) files" + folderSelected)
@@ -47,19 +48,19 @@ def UpdateCol(index, attempt):
 
     #depending on number of attempts changes it to match
     if (attempt == 1):
-        tree.item(id, values=(pubFiles[index], "❌", "✅", "✅"))
+        tree.item(id, values=(pubFiles[index], "❌", "✅", ""))
     elif (attempt == 2):
         tree.item(id, values=(pubFiles[index], "❌", "❌", "✅"))
     elif (attempt == 3):
         tree.item(id, values=(pubFiles[index], "❌", "❌", "❌"))
     else:
-        tree.item(id, values=(pubFiles[index], "✅", "✅", "✅"))
+        tree.item(id, values=(pubFiles[index], "✅", "", ""))
 
 
 #converter, takes pubs and makes them pdfs
 def PubToPdf():
     #once called on re-disables the button so it cant be re-clicked until valid directory and files
-    convertButton.configure(state="disabled")
+    convertButtonPb.configure(state="disabled")
 
     #define the list and strings for the failed/error files
     ErrConvertedFiles = []
@@ -98,7 +99,6 @@ def PubToPdf():
                         #closes the file and clean up
                         doc.Close()
                         gc.collect()
-                        pythoncom.CoUninitialize()
 
                         #changes row colour and updates on gui with before mentioned function
                         if (attempt == 0):
@@ -132,6 +132,80 @@ def PubToPdf():
     #changes message from "all" to "Some" and lists the failed files at the end
     messagebox.showinfo("Files converted", DidError + "files converted successfully\n\n" + ErrConvertedFilesList)
 
+def PdfToDocx():
+    #once called on re-disables the button so it cant be re-clicked until valid directory and files
+    convertButtonWd.configure(state="disabled")
+
+    #define the list and strings for the failed/error files
+    ErrConvertedFiles = []
+    ErrConvertedFilesList = ""
+    DidError = "All "
+    #index for tracking on updateCol function
+    index = -1
+
+    #goes through all files and directories to help convert them
+    for root, dirs, files in os.walk(folderSelected):
+        for file in files:
+            if file.lower().endswith(".pdf"):
+                #moves index to next (for function mentioned before
+                index += 1
+                #resets attempts for each file
+                attempt = 0
+
+                word = win32com.client.Dispatch("Word.Application")
+
+
+                filePathInp = os.path.normpath(os.path.join(root, file))
+                filePathOut = os.path.normpath(os.path.join(root, f"{os.path.splitext(file)[0]}.docx"))
+
+                # while it hasnt failed 4 times
+                while attempt < 3:
+                    print("h1")
+                    try:
+                        print("1")
+                        doc = word.Documents.Open(filePathInp)
+                        # configers the export formats for saving as pdf
+                        print("2")
+                        doc.SaveAs2(
+                            filePathOut,
+                            FileFormat=16
+                        )
+                        print("3")
+                        # closes the file and clean up
+                        doc.Close()
+                        gc.collect()
+
+                        # changes row colour and updates on gui with before mentioned function
+                        if (attempt == 0):
+                            tree.item((tree.get_children()[index]), tags=("success"))
+                        elif (attempt > 0):
+                            tree.item((tree.get_children()[index]), tags=("PartFailed"))
+                        UpdateCol(index, attempt)
+                        break
+
+                    # if error converting
+                    except Exception as e:
+                        # adds 1 to attempts made
+                        attempt += 1
+
+                        # if 3 failes skips file once retrying 3 times and highlights red (rather than green on sucess)
+                        if attempt == 3:
+                            ErrConvertedFiles.append(filePathInp)
+                            tree.item((tree.get_children()[index]), tags=("failed"))
+                            break
+                        # if not failed 3 times yet then retries until either works or failes and skips
+
+    #if any fully failed (3 tries) then sets up output end message to include them
+    if (ErrConvertedFiles):
+        ErrConvertedFilesList = "List of failed file filepaths:"
+        DidError = "Some "
+
+        #makes a string of all the failed files
+        for item in ErrConvertedFiles:
+            ErrConvertedFilesList = ErrConvertedFilesList + "\n" + item
+
+    #changes message from "all" to "Some" and lists the failed files at the end
+    messagebox.showinfo("Files converted", DidError + "files converted successfully\n\n" + ErrConvertedFilesList)
 
 #gui
 try:
@@ -155,11 +229,18 @@ try:
         tree.column(column, width=450 if column == "File" else 50, anchor='w')
     tree.pack()
 
-    label = tk.Label(frame, text="Step 2: Select files to convert too:")
+    label = tk.Label(frame, text="Step 2: Select files to convert too :")
     label.pack()
 
-    convertButton = tk.Button(frame, text="Convert to PDF", command=PubToPdf, state="disabled")
-    convertButton.pack()
+    convertButtonPb = tk.Button(frame, text="Convert to PDF\n.pub -> .pdf", command=PubToPdf, state="disabled")
+    convertButtonPb.pack()
+    #(for publisher to word make sure to convert to pdf first)
+
+    label = tk.Label(frame, text="For publisher to word make sure to convert to pdf first")
+    label.pack()
+
+    convertButtonWd = tk.Button(frame, text="Convert to word\n.pdf -> .docx", command=PdfToDocx, state="disabled")
+    convertButtonWd.pack()
 
     #colour tags for fails and sucesses
     tree.tag_configure("success", background="lightgreen")

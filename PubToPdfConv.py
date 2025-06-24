a3873abbe1cdc2146dfac4d4f1a2abc478ce70c6
l.farnier <lucasfarnier71@gmail.com>
2025-06-24 15:10:57 +0100

    converts to pdf and functional
    now with gui allowing user to visually see issues and such

--- PubToPdfConv.py
+++ PubToPdfConv.py
@@ -1,31 +1,171 @@
 import os
-import comtypes.client
 import win32com.client
+import gc
+import pythoncom
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+os.environ['TCL_LIBRARY'] = r'C:\Users\L.Farnier\AppData\Local\Programs\Python\Python313\tcl\tcl8.6'
 
-publisher = win32com.client.Dispatch("Publisher.Application")
-#publisher.Visible = True
 
-for root, dirs, files in os.walk(r"C:\Test"):
-    for file in files:
-        if file.lower().endswith(".pub"):
-            #gets file path for the input filepath then new file path for output pdf
-            filePathInp = os.path.join(root, file)
-            filePathOut = os.path.join(root, f"{os.path.splitext(file)[0]}.PDF")
+#defining global variabls
+folderSelected = ""
+pubFiles = []
 
-            try:
-                doc = publisher.Open(filePathInp)
-                #configers the export formats for saving as pdf
-                doc.ExportAsFixedFormat(
-                    Filename = filePathOut,
-                    Format = 2,
-                    Intent = 1,
-                    IncludeDocumentProperties = True,
-                    BitmapMissingFonts = True
-                )
-                doc.Close()
 
-                print(f"Conversion successful: {filePathOut}")
+#function for checking folder being picked
+def browseFolders():
+    #globalises to use across multiple
+    global folderSelected
+    global pubFiles
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
-publisher.Quit()
+    #resets the list before refilling it
+    pubFiles = []
+    #remove all items in the gui
+    tree.delete(*tree.get_children())
+    #takes the directory selected and stores it
+    folderSelected = filedialog.askdirectory()
+
+    #checking all files and directories steming from one provided
+    for root, dirs, files in os.walk(folderSelected):
+        for file in files:
+            #if ones found adds it to the list and the gui
+            if file.lower().endswith(".pub"):
+                pubFiles.append(os.path.join(root, file))
+                tree.insert("", "end", values=(os.path.join(root, file), "", "", ""))
+
+    #if the list isnt empty then activate the convert button
+    if len(pubFiles) != 0:
+        convertButton.configure(state="normal")
+    #if its empty output error message box telling user, also keeps convert button inactive
+    else:
+        messagebox.showerror("No files found", "Folder doesnt contain any .pub (publisher) files" + folderSelected)
+
+
+#updates the attempt col of the files listed
+def UpdateCol(index, attempt):
+    id = tree.get_children()[index]
+
+    #depending on number of attempts changes it to match
+    if (attempt == 1):
+        tree.item(id, values=(pubFiles[index], "❌", "✅", "✅"))
+    elif (attempt == 2):
+        tree.item(id, values=(pubFiles[index], "❌", "❌", "✅"))
+    elif (attempt == 3):
+        tree.item(id, values=(pubFiles[index], "❌", "❌", "❌"))
+    else:
+        tree.item(id, values=(pubFiles[index], "✅", "✅", "✅"))
+
+
+#converter, takes pubs and makes them pdfs
+def PubToPdf():
+    #once called on re-disables the button so it cant be re-clicked until valid directory and files
+    convertButton.configure(state="disabled")
+
+    #define the list and strings for the failed/error files
+    ErrConvertedFiles = []
+    ErrConvertedFilesList = ""
+    DidError = "All "
+    #index for tracking on updateCol function
+    index = -1
+
+    #goes through all files and directories to help convert them
+    for root, dirs, files in os.walk(folderSelected):
+        for file in files:
+            if file.lower().endswith(".pub"):
+                #moves index to next (for function mentioned before
+                index += 1
+                #resets attempts for each file
+                attempt = 0
+
+                publisher = win32com.client.Dispatch("Publisher.Application")
+
+                #gets file path for the input filepath then new file path for output pdf
+                filePathInp = os.path.normpath(os.path.join(root, file))
+                filePathOut = os.path.normpath(os.path.join(root, f"{os.path.splitext(file)[0]}.pdf"))
+
+                #while it hasnt failed 4 times
+                while attempt < 3:
+                    try:
+                        doc = publisher.Open(filePathInp)
+                        #configers the export formats for saving as pdf
+                        doc.ExportAsFixedFormat(
+                            Filename = filePathOut,
+                            Format = 2,
+                            Intent = 1,
+                            IncludeDocumentProperties = True,
+                            BitmapMissingFonts = True
+                        )
+                        #closes the file and clean up
+                        doc.Close()
+                        gc.collect()
+                        pythoncom.CoUninitialize()
+
+                        #changes row colour and updates on gui with before mentioned function
+                        if (attempt == 0):
+                            tree.item((tree.get_children()[index]), tags=("success"))
+                        elif (attempt > 0):
+                            tree.item((tree.get_children()[index]), tags=("PartFailed"))
+                        UpdateCol(index, attempt)
+                        break
+
+                    #if error converting
+                    except Exception as e:
+                        #adds 1 to attempts made
+                        attempt += 1
+
+                        #if 3 failes skips file once retrying 3 times and highlights red (rather than green on sucess)
+                        if attempt == 3:
+                            ErrConvertedFiles.append(filePathInp)
+                            tree.item((tree.get_children()[index]), tags=("failed"))
+                            break
+                        #if not failed 3 times yet then retries until either works or failes and skips
+
+    #if any fully failed (3 tries) then sets up output end message to include them
+    if (ErrConvertedFiles):
+        ErrConvertedFilesList = "List of failed file filepaths:"
+        DidError = "Some "
+
+        #makes a string of all the failed files
+        for item in ErrConvertedFiles:
+            ErrConvertedFilesList = ErrConvertedFilesList + "\n" + item
+
+    #changes message from "all" to "Some" and lists the failed files at the end
+    messagebox.showinfo("Files converted", DidError + "files converted successfully\n\n" + ErrConvertedFilesList)
+
+
+#gui
+try:
+    root = tk.Tk()
+    root.title("Pub to PDF")
+
+    frame = tk.Frame(root, padx=20, pady=20)
+    frame.pack()
+
+
+    label = tk.Label(frame, text="Step 1: Select folder with .pub files in and pdf copies will be made:")
+    label.pack()
+
+    browseButton = tk.Button(frame, text="Browse", command=browseFolders)
+    browseButton.pack()
+
+    columns = ("File", "Att 1", "Att 2", "Att 3")
+    tree = ttk.Treeview(frame, columns=columns, show="headings")
+    for column in columns:
+        tree.heading(column, text=column)
+        tree.column(column, width=450 if column == "File" else 50, anchor='w')
+    tree.pack()
+
+    label = tk.Label(frame, text="Step 2: Select files to convert too:")
+    label.pack()
+
+    convertButton = tk.Button(frame, text="Convert to PDF", command=PubToPdf, state="disabled")
+    convertButton.pack()
+
+    #colour tags for fails and sucesses
+    tree.tag_configure("success", background="lightgreen")
+    tree.tag_configure("partFailed", background="lightcoral")
+    tree.tag_configure("failed", background="#ff5733")
+
+    root.mainloop()
+except Exception:
+    print("ended")

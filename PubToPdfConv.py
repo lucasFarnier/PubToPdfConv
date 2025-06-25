57a4a8954674895af38812e3f649d93bbda9dcf5
l.farnier <lucasfarnier71@gmail.com>
2025-06-25 10:25:07 +0100

    can now convert from pdf to docx (can do publisher to docx by doing pub-pdf-docx)
    made more efficienct
    highlights failed and sucesseful files

--- PubToPdfConv.py
+++ PubToPdfConv.py
@@ -1,7 +1,6 @@
 import os
 import win32com.client
 import gc
-import pythoncom
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 os.environ['TCL_LIBRARY'] = r'C:\Users\L.Farnier\AppData\Local\Programs\Python\Python313\tcl\tcl8.6'
@@ -14,7 +13,7 @@ pubFiles = []
 
 #function for checking folder being picked
 def browseFolders():
-    #globalises to use across multiple
+    #globalises to use across multiple functions
     global folderSelected
     global pubFiles
 
@@ -42,6 +41,14 @@ def browseFolders():
         messagebox.showerror("No files found", "Folder doesnt contain any .pub (publisher) files" + folderSelected)
 
 
+def toPdf():
+    PubToPdfAndPdfToDocx("pub", "pdf")
+
+
+def toDocx():
+    PubToPdfAndPdfToDocx("pdf", "docx")
+
+
 #updates the attempt col of the files listed
 def UpdateCol(index, attempt):
     id = tree.get_children()[index]
@@ -57,9 +64,10 @@ def UpdateCol(index, attempt):
         tree.item(id, values=(pubFiles[index], "✅", "", ""))
 
 
-#converter, takes pubs and makes them pdfs
-def PubToPdf():
+#converter, takes pubs and makes them pdfs or takes pdfs and makes them docx
+def PubToPdfAndPdfToDocx(convertFrom, convertTo):
     #once called on re-disables the button so it cant be re-clicked until valid directory and files
+    convertButtonWd.configure(state="disabled")
     convertButtonPb.configure(state="disabled")
 
     #define the list and strings for the failed/error files
@@ -72,30 +80,47 @@ def PubToPdf():
     #goes through all files and directories to help convert them
     for root, dirs, files in os.walk(folderSelected):
         for file in files:
-            if file.lower().endswith(".pub"):
-                #moves index to next (for function mentioned before
+            if file.lower().endswith(".pub") or file.lower().endswith(".pdf"):
+                # moves index to next (for function mentioned before)
                 index += 1
+                if not(file.lower().endswith("." + convertFrom)):
+                    tree.item((tree.get_children()[index]), tags=("NA"))
+
+
+            if file.lower().endswith("." + convertFrom):
                 #resets attempts for each file
                 attempt = 0
 
-                publisher = win32com.client.Dispatch("Publisher.Application")
+                if convertFrom == "pub":
+                    publisherWord = win32com.client.Dispatch("Publisher.Application")
+                elif convertFrom == "pdf":
+                    publisherWord = win32com.client.Dispatch("Word.Application")
+
 
                 #gets file path for the input filepath then new file path for output pdf
                 filePathInp = os.path.normpath(os.path.join(root, file))
-                filePathOut = os.path.normpath(os.path.join(root, f"{os.path.splitext(file)[0]}.pdf"))
+                filePathOut = os.path.normpath(os.path.join(root, f"{os.path.splitext(file)[0]}."+convertTo))
 
                 #while it hasnt failed 4 times
                 while attempt < 3:
                     try:
-                        doc = publisher.Open(filePathInp)
                         #configers the export formats for saving as pdf
-                        doc.ExportAsFixedFormat(
-                            Filename = filePathOut,
-                            Format = 2,
-                            Intent = 1,
-                            IncludeDocumentProperties = True,
-                            BitmapMissingFonts = True
-                        )
+                        if convertTo == "pdf":
+                            doc = publisherWord.Open(filePathInp)
+                            doc.ExportAsFixedFormat(
+                                Filename = filePathOut,
+                                Format = 2,
+                                Intent = 1,
+                                IncludeDocumentProperties = True,
+                                BitmapMissingFonts = True
+                            )
+                        if convertTo == "docx":
+                            doc = publisherWord.Documents.Open(filePathInp)
+                            doc.SaveAs2(
+                                filePathOut,
+                                FileFormat=16
+                            )
+
                         #closes the file and clean up
                         doc.Close()
                         gc.collect()
@@ -117,83 +142,9 @@ def PubToPdf():
                         if attempt == 3:
                             ErrConvertedFiles.append(filePathInp)
                             tree.item((tree.get_children()[index]), tags=("failed"))
+                            UpdateCol(index, attempt)
                             break
                         #if not failed 3 times yet then retries until either works or failes and skips
-
-    #if any fully failed (3 tries) then sets up output end message to include them
-    if (ErrConvertedFiles):
-        ErrConvertedFilesList = "List of failed file filepaths:"
-        DidError = "Some "
-
-        #makes a string of all the failed files
-        for item in ErrConvertedFiles:
-            ErrConvertedFilesList = ErrConvertedFilesList + "\n" + item
-
-    #changes message from "all" to "Some" and lists the failed files at the end
-    messagebox.showinfo("Files converted", DidError + "files converted successfully\n\n" + ErrConvertedFilesList)
-
-def PdfToDocx():
-    #once called on re-disables the button so it cant be re-clicked until valid directory and files
-    convertButtonWd.configure(state="disabled")
-
-    #define the list and strings for the failed/error files
-    ErrConvertedFiles = []
-    ErrConvertedFilesList = ""
-    DidError = "All "
-    #index for tracking on updateCol function
-    index = -1
-
-    #goes through all files and directories to help convert them
-    for root, dirs, files in os.walk(folderSelected):
-        for file in files:
-            if file.lower().endswith(".pdf"):
-                #moves index to next (for function mentioned before
-                index += 1
-                #resets attempts for each file
-                attempt = 0
-
-                word = win32com.client.Dispatch("Word.Application")
-
-
-                filePathInp = os.path.normpath(os.path.join(root, file))
-                filePathOut = os.path.normpath(os.path.join(root, f"{os.path.splitext(file)[0]}.docx"))
-
-                # while it hasnt failed 4 times
-                while attempt < 3:
-                    print("h1")
-                    try:
-                        print("1")
-                        doc = word.Documents.Open(filePathInp)
-                        # configers the export formats for saving as pdf
-                        print("2")
-                        doc.SaveAs2(
-                            filePathOut,
-                            FileFormat=16
-                        )
-                        print("3")
-                        # closes the file and clean up
-                        doc.Close()
-                        gc.collect()
-
-                        # changes row colour and updates on gui with before mentioned function
-                        if (attempt == 0):
-                            tree.item((tree.get_children()[index]), tags=("success"))
-                        elif (attempt > 0):
-                            tree.item((tree.get_children()[index]), tags=("PartFailed"))
-                        UpdateCol(index, attempt)
-                        break
-
-                    # if error converting
-                    except Exception as e:
-                        # adds 1 to attempts made
-                        attempt += 1
-
-                        # if 3 failes skips file once retrying 3 times and highlights red (rather than green on sucess)
-                        if attempt == 3:
-                            ErrConvertedFiles.append(filePathInp)
-                            tree.item((tree.get_children()[index]), tags=("failed"))
-                            break
-                        # if not failed 3 times yet then retries until either works or failes and skips
 
     #if any fully failed (3 tries) then sets up output end message to include them
     if (ErrConvertedFiles):
@@ -232,20 +183,21 @@ try:
     label = tk.Label(frame, text="Step 2: Select files to convert too :")
     label.pack()
 
-    convertButtonPb = tk.Button(frame, text="Convert to PDF\n.pub -> .pdf", command=PubToPdf, state="disabled")
+    convertButtonPb = tk.Button(frame, text="Convert to PDF\n.pub -> .pdf", command=toPdf, state="disabled")
     convertButtonPb.pack()
     #(for publisher to word make sure to convert to pdf first)
 
     label = tk.Label(frame, text="For publisher to word make sure to convert to pdf first")
     label.pack()
 
-    convertButtonWd = tk.Button(frame, text="Convert to word\n.pdf -> .docx", command=PdfToDocx, state="disabled")
+    convertButtonWd = tk.Button(frame, text="Convert to word\n.pdf -> .docx", command=toDocx, state="disabled")
     convertButtonWd.pack()
 
     #colour tags for fails and sucesses
-    tree.tag_configure("success", background="lightgreen")
-    tree.tag_configure("partFailed", background="lightcoral")
-    tree.tag_configure("failed", background="#ff5733")
+    tree.tag_configure("success", background="#ADEA33")
+    tree.tag_configure("partFailed", background="#EAB333")
+    tree.tag_configure("failed", background="#E14B2A")
+    tree.tag_configure("NA", foreground="#B1B1B1")
 
     root.mainloop()
 except Exception:
